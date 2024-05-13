cd7d258c1c266960f64f058da1ef27bd8f9bb623
Saira Batool <sairabatool787@gmail.com>
2024-05-13 06:28:14 +0500

    Done

---
+++ Chap#2/app.js
@@ -0,0 +1,30 @@
+// 1.
+var username;
+// 2.
+var myName = "Saira_Batool";
+// 3.
+var message = "Hello World";
+alert(message)
+// 4.
+alert("My name is Saira Batool");
+alert("I have learn Html and Css");
+alert("Now I'm working on JavaScript");
+
+// 5.
+var anyname = alert("PIZZA\nPIZZ\nPIZ\nPI\nI");
+
+
+// 6.
+var email = "sairabatool787@gmail.com";
+alert("My email address is " +  email);
+
+// 7.
+var book = " A smatter way to learn JavaScript"
+alert("I am trying to learn from the Book" + book);
+
+// 8.
+document.write("Yah! I can write HTML content through JavaScript");
+
+// 9.
+var sign = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(sign);
---
+++ Chap#3/app.js
@@ -0,0 +1,20 @@
+// 1.
+var age = 20;
+alert("I am " + age + " years old");
+
+// 2.
+var visitors = 0;
+alert("You have visited this site 14 times");
+
+// 3.
+var birthYear = 2003;
+document.write("My birth year is " + birthYear + "</br>");
+document.write("Data type of my declared variable is number");
+
+// 4.
+var store = "www.xyzClothing.com";
+var visitorName = prompt("Kindly Added Your Name:")
+var productTitle = prompt("You have Ordered Product")
+var quantity = +prompt("Enter a Quantity of Product");
+
+document.write(visitorName + " ordered " + quantity + " " + productTitle + " on " + store);
