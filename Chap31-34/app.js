b8cc0636f254ee4650e8ce00c7335df60ce7a954
Saira Batool <sairabatool787@gmail.com>
2024-07-24 11:51:13 +0500

    In Process

---
+++ Chap31-34/app.js
@@ -0,0 +1,48 @@
+// 1.
+// var currentDate = new Date()
+// document.write(currentDate)
+
+// // 2.
+// // var dateIs = new Date()
+// // date = dateIs.getMonth()
+// // document.write(date)
+
+// var monthHere = ["jan","feb","march","april","may","june","july","Aug","sep","oct","nov","dec"];
+// var monthIs = monthHere.getMonth()
+// for (let i = 0; i < monthHere.length; i++) {
+//     var element = mHereonth[i];
+//     element = monthIs
+    
+// }
+// console.log(monthIs)
+
+// // 3.
+// new day = new Date()
+// // dayIs = day.getDay()
+// // if(dayIs >= 3){
+// document.write(day.setDay(0))
+// // }
+
+// // 8.
+// var laterDate = new Date("11 31, 2020")
+// document.write(laterDate)
+
+// // 9.
+// var recDate = new Date()
+// var ramadanDate = new Date("June 18,2015")
+// var diff = recDate.getTime()-ramadanDate.getTime()
+// var res = diff/(1000*60*60*24)
+// console.log(Math.round(res) + " days have passed since 1st Ramadan, 2015")
+
+// // 10.
+// var firstDate = new Date()
+// var secondDate = new Date("Jan 1,2015")
+// diff = firstDate.getTime()-secondDate.getTime()
+// var res = diff/1000
+// document.write("on reference date " + firstDate + " " + Math.round(res) + " seconds had passed since begining of 2015")
+
+// // 11.
+// var fisDate = new Date()
+// var res = fisDate.getHours()
+// var final = (res - 1)
+// document.write(final)
--- Chap38-42/app.js
+++ Chap38-42/app.js
@@ -15,17 +15,48 @@
 // }
 // var ret = +prompt("Enter a year")
 // if(ret % 4==0){
-//     document.write("It is a leap Year")
+//     document.write(leapYear("It is a leap Year"))
 // }else{
-//     document.write("It is not a leap Year")
+//     document.write(leapYear("It is not a leap Year"))
 // }
 
 // 3.
-function areaOfTriangle(a,b,c){
-    var S = 1
-    var area = S*(S-a)*(S-b)*(S-c)
-    return area;
-}
-var result = areaOfTriangle(2,3,5)
-console.log(result)
+// function areaOfTriangle(a,b,c){
+//     var S = 1
+//     var area = S*(S-a)*(S-b)*(S-c)
+//     return area;
+// }
+// var result = areaOfTriangle(2,3,5)
+// console.log(result)
 
+// 4.
+// var a = +prompt("Enter a subj1 marks")
+// var b = +prompt("Enter a subj2 marks")
+// var c = +prompt("Enter a subj3 marks")
+
+// function marks(math, phy, isl) {
+//     function average(subj1, subj2, subj3) {
+//         var average = totalmarks / 3
+//         return marks;
+//     }
+//     function percantage(math, phy, isl) {
+//         var totalmarks = (subj1 + subj2 + subj3) * 100 / 300
+//         return totalmarks
+//     }
+// }
+//  var res = marks(a + b + c)
+// console.log(res)
+// marks()
+
+// 4.
+
+
+// 5.
+function customFuntion(string, letter) {
+    for (let i = 0; i < string.length; i++){
+        if (string[i] === letter) {
+            return i
+        }
+} return -1
+}
+alert(customFuntion("Hello World", "H"))
