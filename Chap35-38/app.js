970ed7810433929461f53b1b830e4cc66fda5f3a
Saira Batool <sairabatool787@gmail.com>
2024-07-28 19:30:14 +0500

    Done

---
+++ Chap35-38/app.js
@@ -0,0 +1,114 @@
+// // 1.
+
+// function currentDate(){
+//     var date = new Date();
+//     return date
+// }
+// document.write(currentDate())
+
+// // 2.
+// function fullName(fName,lName){
+// var fullName = fName + " " + lName
+//     return fullName
+// }
+// var a = prompt("Please enter first name")
+// var b = prompt("please enter last name")
+// document.write("Hi " + fullName(a,b))
+
+// // 3.
+// function addition(a,b){
+// var sum = a + b;
+// return sum
+// }
+// var num1 = +prompt("Enter a first Number")
+// var num2 = +prompt("Enter a second Number")
+// document.write(addition(num1,num2))
+
+// // 4.
+// function calculator(num1,operator,num2){ 
+// if(operator == "+"){
+//     return num1 + num2
+// }else if(operator == "-"){
+//     return num1-num2
+// }else if(operator == "*"){
+//     return num1*num2
+// }else if(operator == "/"){
+//     return num1%num2
+// }else{
+// return "Invalid Operator"
+// }
+// }
+// var a = +prompt("Enter a first Number")
+// var b = prompt("Enter a Operator +,-,/,*,%")
+// var c = +prompt("Enter a second Number")
+// document.write(calculator(a,b,c))
+
+// // 5.
+// function square(number){
+//     var num = number*number
+//     return num;
+
+// }
+// var result = +prompt("enter a number")
+// document.write(square(result))
+
+// // 6.
+// function counting(start,end){
+
+// // 7. 
+// for (let i = start; i <= end; i++) {
+//     console.log(i)
+
+// }
+// var a = +prompt("enter a number start from")
+// var b = +prompt("enter a number end from")
+// counting(a,b)
+
+// 8.
+// function calculateHypotenuse(Hypo) {
+//     return Hypo
+//     function calculateSquare(Base, Perpend) {
+//         return Base * Base + Perpend * Perpend
+//     }
+
+// }
+// var a = prompt("Enter a base number")
+// var b = prompt("enter a perpendicular number")
+// document.write(calculateHypotenuse(a, b))
+
+// // 9.
+// i.
+// function areaOfRectangle(width,height){
+//     return width*height
+// }
+// var a = +prompt("Enter a width of a triangle")
+// var b = +prompt("Enter a length of a triangle")
+// document.write(areaOfRectangle(a,b))
+// ii
+// function area(a,b){
+// return a*b
+// }
+// document.write(area(2,4))
+
+// 10.
+// function isPalindrome(str){
+// for (let i = 0; i < str.length; i++) {    
+//     var element = str[i] // + str[i+1]//str[i]//str.slice(1)//
+//     break
+// }
+//     var newstr = str.slice(-1)
+
+// if(element === newstr){
+// return alert("It is plendrome word")
+// }else{
+//     return alert("It is not plendrome")
+// }
+
+// }
+// var string = prompt("Enter a polidrome word")
+// isPalindrome(string)
+
+// 11.
+function upperCase(){
+    if(str.indexOf(" " + 1).toUpperCase())
+}
---
+++ Chap43-48/app.js
@@ -0,0 +1,17 @@
+function showalert(){
+    alert("Thanks For Purchasing!!")
+}
+
+function deleteRow(){
+    // FOR LINE ONE 
+var table0 = document.getElementById("zero0").style.display="none"
+var table0 = document.getElementById("zero1").style.display="none"
+var table0 = document.getElementById("zero2").style.display="none"
+var table0 = document.getElementById("zero3").style.display="none"
+// FOR LINE TWO 
+var table1 = document.getElementById("one0").style.display="none"
+var table1 = document.getElementById("one1").style.display="none"
+var table1 = document.getElementById("one2").style.display="none"
+var table1 = document.getElementById("one3").style.display="none"
+
+}
