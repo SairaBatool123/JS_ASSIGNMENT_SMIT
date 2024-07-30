355a4de83033a9e60e42e1434a2b0687cbdfa9cf
Saira Batool <sairabatool787@gmail.com>
2024-07-30 13:30:00 +0500

    Done

--- Chap43-48/app.js
+++ Chap43-48/app.js
@@ -1,17 +1,24 @@
-function showalert(){
+// 2.
+function showalert() {
     alert("Thanks For Purchasing!!")
 }
 
-function deleteRow(){
+function deleteRow() {
     // FOR LINE ONE 
-var table0 = document.getElementById("zero0").style.display="none"
-var table0 = document.getElementById("zero1").style.display="none"
-var table0 = document.getElementById("zero2").style.display="none"
-var table0 = document.getElementById("zero3").style.display="none"
-// FOR LINE TWO 
-var table1 = document.getElementById("one0").style.display="none"
-var table1 = document.getElementById("one1").style.display="none"
-var table1 = document.getElementById("one2").style.display="none"
-var table1 = document.getElementById("one3").style.display="none"
+    // var table0 = document.getElementById("zero0").style.display="none"
+    // var table1 = document.getElementById("one3").style.display="none"
+    document.getElementById("zero").innerHTML = ''
+    document.getElementById("one").innerHTML = ''
+    document.getElementById("two").innerHTML = ''
+    document.getElementById("three").innerHTML = ''
 
+}
+
+// 5.
+var counter = 0
+function increase() {
+    var incr = document.getElementById("counter").innerHTML=counter++
+}
+function decrease(){
+    var decr = document.getElementById("counter").innerHTML=counter--
 }
